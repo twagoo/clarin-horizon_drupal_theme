8915a64bd2b0d4fa6e648f58c0662dc362413e28
André Moreira <andre@clarin.eu>
2017-06-09 19:05:10 +0200

    Added login page customisations.

--- src/theme/template.php
+++ src/theme/template.php
@@ -50,4 +50,26 @@ function CLARIN_Horizon_preprocess_html(&$vars) {
 function CLARIN_Horizon_menu_link__menu_block($variables) {
 	return theme_menu_link($variables);
 }
+
+/**  
+ * Custumise login page
+ */     
+function CLARIN_Horizon_theme() {      
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+  'render element' => 'form',        
+  'path' => drupal_get_path('theme', 'CLARIN_Horizon') . '/templates',
+  'template' => 'user-login',                    
+  'preprocess functions' => array(  
+  'CLARIN_Horizon_preprocess_user_login'
+  ),                 
+ );                        
+return $items;                                                                                                               
+}                           
+                          
+function CLARIN_Horizon_form_user_login_alter(&$form, $form_state) {
+    $form['name']['#title'] = 'E-mail address';
+}
+
 ?>
---
+++ src/theme/templates/user-login.tpl.php
@@ -0,0 +1,10 @@
+<div class="region region-content">
+<br/>
+<big>
+<p><span class="glyphicon glyphicon-warning-sign">&nbsp;</span>
+This login page leads to the <strong>CLARIN intranet</strong>. If you are trying to access one of the CLARIN corpora or tools instead, please see <a href=../content/easy-access-protected-resources>Easy access to protected resources</a>.</p>
+</big>
+<br/>
+</div>
+
+<?php print drupal_render_children($form) ?>
