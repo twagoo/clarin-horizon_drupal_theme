79f026239eecfba3c49b179a7771b58e728a676b
André Moreira <andre@clarin.eu>
2018-03-29 15:15:59 +0200

    Added bootstrap-toc to theme
    See: https://afeld.github.io/bootstrap-toc/

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PACKAGE_VERSION="0.1.1"
+PACKAGE_VERSION="0.1.2"
 DRUPAL_BOOTSTRAP_VERSION="7.x-3.12"
 BASE_STYLE_REPOSITORY="https://github.com/clarin-eric/base_style"
 BASE_STYLE_VERSION="0.2.0"
---
+++ src/theme/js/toc-clarineu.js
@@ -0,0 +1,17 @@
+(function($) {
+  'use strict';
+
+  $(document).ready(function($) {
+    $(function() {
+      var navSelector = '#toc';
+      var scopeSelector = '#tocscope';
+      Toc.init({
+        $nav: $(navSelector),
+        $scope: $(scopeSelector)
+      });
+      $('body').scrollspy({
+        target: navSelector
+      });
+    });
+  });
+})(jQuery);
