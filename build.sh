b0c45f8a3f405bdb08823e00f43271c8068c5eb4
André Moreira <andre@clarin.eu>
2018-05-01 11:53:55 +0200

    Added forgotten bootstrap-toc.css

--- build.sh
+++ build.sh
@@ -62,7 +62,6 @@ mv -f -- "${OUTPUT_DIRECTORY}/basestyle/js/" "${BUILD_DIRECTORY}/js/bootstrap"
 
 echo 'Compiling LESS...'
 ## Compile style from basedstyle less folder
-mkdir -p "${BUILD_DIRECTORY}/css"
 ${LESSC} "${OUTPUT_DIRECTORY}/basestyle/less/style.less" --clean-css='--s0' > "${BUILD_DIRECTORY}/css/style.css"
 
 echo 'Packaging...'
